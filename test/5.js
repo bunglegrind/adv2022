b2564655c353d6cbf7795d85c36987fe5d437742
bunglegrind <bunglegrind@libero.it>
2022-12-05 07:08:23 +0100

    day 5 setup

--- test/5.js
+++ test/5.js
@@ -1,2 +1,12 @@
 import test from "tape";
 import libs from "../test/5.js";
+
+const sample = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+ move 1 from 2 to 1
+ move 3 from 1 to 3
+ move 2 from 2 to 1
+ move 1 from 1 to 2`;
