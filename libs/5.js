9e606d44b5f1fda270602ff0a33b29aef8bed7fe
bunglegrind <bunglegrind@libero.it>
2022-12-05 15:01:10 +0100

    day 5 completed

--- libs/5.js
+++ libs/5.js
@@ -1,11 +1,143 @@
 import * as R from "ramda";
 
-const parseInt = R.pipe(R.split("\n"));
+function stack() {
+    const st = [];
+
+    return Object.freeze({
+        size: () => st.length,
+        push: R.tap((x) => st.push(x)),
+        pop: () => st.pop(),
+        top: () => R.last(st)
+    });
+}
+
+stack.of = function (array) {
+    const st = stack();
+    array.forEach(function (x) {
+        st.push(x);
+    });
+    return st;
+};
+
+function parseAction(action) {
+    const matched = /^move (\d+) from (\d+) to (\d+)$/.exec(action.trim());
+    return (
+        matched.length
+        ? Object.freeze({
+            repeat: Number(matched[1]),
+            from: Number(matched[2]),
+            to: Number(matched[3])
+        })
+        : Object.create(null)
+    );
+}
+
+function crane(version = 9000) {
+    const crates = [];
+    const performOnCrates = (f) => () => R.reduce(
+        (acc, st) => acc.concat([st[f]()]),
+        [],
+        crates
+    );
+
+    function performAction({from, to, repeat}) {
+        from -= 1;
+        to -= 1;
+        repeat -= 1;
+        
+        function recursive_move(repeat, from, to) {
+            to.push(from.pop());
+            return (
+                repeat
+                ? recursive_move(repeat - 1, from, to)
+                : ""
+            );
+        }
+
+        function block_move(repeat, from, to) {
+            const tmp = stack();
+            recursive_move(repeat, from, tmp);
+            recursive_move(repeat, tmp, to);
+        }
+
+        if (version === 9000) {
+            recursive_move(repeat, crates[from], crates[to]);
+        } else {
+            block_move(repeat, crates[from], crates[to]);
+        }
+    };
+
+    return Object.freeze({
+        addCrate: (stack) => crates.push(stack),
+        performAction,
+        performActions: R.forEach(function (action) {
+            performAction(action);
+        }),
+        top: performOnCrates("top"),
+        sizes: performOnCrates("size"),
+        size: () => crates.length
+    });
+}
+
+const parseInput = (version) => R.pipe(
+    R.split("\n\n"),
+    function ([craneToParse, actionsToParse]) {
+        const craneSize = 3;
+        const craneSpace = 1;
+        const actions = R.pipe(
+            R.split("\n"),
+            R.reject(R.isEmpty),
+            R.map(parseAction)
+        )(actionsToParse);
+        
+        const tmp = R.split("\n", craneToParse);
+        const summary = R.last(tmp);
+        const rows = R.reverse(R.init(tmp));
+        const maxCrane = Number(summary.match(/\d+\s*$/)[0]);
+        const indices = R.times(
+            (n) => (craneSize + craneSpace) * n + craneSpace,
+            maxCrane
+        );
+        const crates = [];
+        const cr = crane(version);
+        
+        rows.forEach(function (row) {
+            indices.forEach(function (i) {
+                if (!crates[i]) {
+                    crates[i] = stack();
+                    cr.addCrate(crates[i]);
+                }
+                if (row[i].trim()) {
+                    crates[i].push(row[i]);
+                }
+            });            
+        });
+
+        return Object.freeze({actions, crane: cr});
+    }
+);
 
 export default Object.freeze({
-    parseInt,
+    parseInput,
+    parseAction,
+    stack,
+    crane,
     exec: {
-        a: 0,
-        b: 0
+        a: R.pipe(
+            parseInput(9000),
+            function ({crane, actions}) {
+                crane.performActions(actions);
+                return crane.top();
+            },
+            R.join("")
+        ),
+        b: R.pipe(
+            parseInput(9001),
+            function ({crane, actions}) {
+                crane.performActions(actions);
+                return crane.top();
+            },
+            R.join("")
+        )
     }
 });
--- test/5.js
+++ test/5.js
@@ -1,5 +1,5 @@
 import test from "tape";
-import libs from "../test/5.js";
+import libs from "../libs/5.js";
 
 const sample = `    [D]    
 [N] [C]    
@@ -10,3 +10,104 @@ const sample = `    [D]
  move 3 from 1 to 3
  move 2 from 2 to 1
  move 1 from 1 to 2`;
+
+
+test("create an empty stack", function (t) {
+    const stack = libs.stack();
+    t.equal(stack.size(), 0);
+    t.end();
+});
+
+
+test("push and pop on a stack", function (t) {
+    const stack = libs.stack();
+    t.equal(stack.push("R"), "R");
+    t.equal(stack.push("Q"), "Q");
+    t.equal(stack.size(), 2);
+    t.equal(stack.pop(), "Q");
+    t.equal(stack.size(), 1);
+    t.equal(stack.pop(), "R");
+    t.equal(stack.size(), 0);
+    t.end();
+});
+
+test("reading the top a stack", function (t) {
+    const stack = libs.stack();
+    stack.push("R");
+    stack.push("Q");
+    t.equal(stack.top(), "Q");
+    t.equal(stack.top(), "Q");
+    stack.pop();
+    t.equal(stack.top(), "R");
+    t.end();
+});
+
+
+test("create stack from array", function (t) {
+    const stack = libs.stack.of(["A", "B", "C"]);
+    t.equal(stack.size(), 3);
+    t.equal(stack.top(), "C");
+    t.end();
+});
+
+test("read an action", function (t) {
+    t.deepEqual(
+        libs.parseAction("move 1 from 2 to 1"), {from: 2, to: 1, repeat: 1}
+    );
+    t.deepEqual(
+        libs.parseAction("move 4 from 12 to 134"), {from: 12, to: 134, repeat: 4}
+    );
+    t.end();
+});
+
+test("move crane according to actions", function (t) {
+    const crane = libs.crane();
+    crane.addCrate(libs.stack.of(["A", "B", "C"]));
+    crane.addCrate(libs.stack.of(["D", "E"]));
+    crane.addCrate(libs.stack.of(["F"]));
+    crane.performAction({from: 1, to: 2, repeat: 1});
+    t.deepEqual(crane.top(), ["B", "C", "F"]);
+    t.deepEqual(crane.sizes(), [2, 3, 1]);
+    crane.performAction({from: 2, to: 3, repeat: 2});
+    t.deepEqual(crane.top(), ["B", "D", "E"]);
+    t.deepEqual(crane.sizes(), [2, 1, 3]);
+    t.end();
+});
+
+test("parse input correctly", function (t) {
+    const {crane, actions} = libs.parseInput(9000)(sample);
+    t.equal(crane.size(), 3);
+    t.deepEqual(crane.top(), ["N", "D", "P"]);
+    t.deepEqual(crane.sizes(), [2, 3, 1]);
+    t.equal(actions.length, 4);
+    t.deepEqual(actions[0], {from: 2, to: 1, repeat: 1});
+    t.deepEqual(actions[1], {from: 1, to: 3, repeat: 3});
+    t.deepEqual(actions[2], {from: 2, to: 1, repeat: 2});
+    t.deepEqual(actions[3], {from: 1, to: 2, repeat: 1});
+    t.end();
+});
+
+test("sample return expect outcome A", function (t) {
+    t.equal(libs.exec.a(sample), "CMZ");
+    t.end();
+});
+
+
+test("move crane 9001 according to actions", function (t) {
+    const crane = libs.crane(9001);
+    crane.addCrate(libs.stack.of(["A", "B", "C"]));
+    crane.addCrate(libs.stack.of(["D", "E"]));
+    crane.addCrate(libs.stack.of(["F"]));
+    crane.performAction({from: 1, to: 2, repeat: 1});
+    t.deepEqual(crane.top(), ["B", "C", "F"]);
+    t.deepEqual(crane.sizes(), [2, 3, 1]);
+    crane.performAction({from: 2, to: 3, repeat: 2});
+    t.deepEqual(crane.top(), ["B", "D", "C"]);
+    t.deepEqual(crane.sizes(), [2, 1, 3]);
+    t.end();
+});
+
+test("sample return expect outcome B", function (t) {
+        t.equal(libs.exec.b(sample), "MCD");
+        t.end();
+});
