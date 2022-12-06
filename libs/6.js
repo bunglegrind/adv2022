da59e32629d81638af09ff182064e85c8a2bbb3a
bunglegrind <bunglegrind@libero.it>
2022-12-06 07:17:39 +0100

    day 6 WIP

---
+++ libs/6.js
@@ -0,0 +1,9 @@
+import * as R from "ramda";
+
+
+export default Object.freeze({
+    exec: {
+        a: 0,
+        b: 0
+    }
+});
---
+++ test/6.js
@@ -0,0 +1,11 @@
+import test from "tape";
+import libs from "../libs/6.js";
+
+const samples = [
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+    "bvwbjplbgvbhsrlpgdmjqwftvncz",
+    "nppdvjthqldpwncqszvftbrmjlhg",
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+];
+
