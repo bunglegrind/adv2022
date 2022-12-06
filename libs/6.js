f8e1bae7ac2743a44e3e520c49b1adb7ade531bb
bunglegrind <bunglegrind@libero.it>
2022-12-06 09:27:33 +0100

    day 6 completed

--- libs/6.js
+++ libs/6.js
@@ -1,9 +1,29 @@
 import * as R from "ramda";
 
+const isMarker = (symbols) => R.uniq(symbols).length === symbols.length;
+
+const markerLength = 4;
+
+
+
+function finder(markerLength) {
+    return function findMarker(stream, pos = markerLength) {
+        return (
+            isMarker(stream.slice(0, markerLength))
+            ? pos
+            : findMarker(stream.slice(1), pos + 1)
+        );
+    };
+}
+
+const parseInput = R.split("");
 
 export default Object.freeze({
+    parseInput,
+    isMarker,
+    finder,
     exec: {
-        a: 0,
-        b: 0
+        a: R.pipe(parseInput, finder(4)),
+        b: R.pipe(parseInput, finder(14)),
     }
 });
--- test/6.js
+++ test/6.js
@@ -9,3 +9,39 @@ const samples = [
     "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 ];
 
+
+test("a sequence is composed by distint symbols", function (t) {
+    t.ok(libs.isMarker(["a", "b", "c", "d"]));
+    t.notOk(libs.isMarker(["a", "c", "c", "d"]));
+    t.end();
+});
+
+
+test("imput is parsed correctly", function (t) {
+    t.deepEqual(libs.parseInput("asdasd"), ["a", "s", "d", "a", "s", "d"]);
+    t.end();
+});
+
+
+test("find start marker", function (t) {
+    t.equal(libs.finder(4)(["a", "a", "b", "c", "d", "e"]), 5);
+    t.end();
+});
+
+test("sample return expect outcome A", function (t) {
+        t.equal(libs.exec.a(samples[0]), 7);
+        t.equal(libs.exec.a(samples[1]), 5);
+        t.equal(libs.exec.a(samples[2]), 6);
+        t.equal(libs.exec.a(samples[3]), 10);
+        t.equal(libs.exec.a(samples[4]), 11);
+        t.end();
+});
+
+test("sample return expect outcome B", function (t) {
+            t.equal(libs.exec.b(samples[0]), 19);
+            t.equal(libs.exec.b(samples[1]), 23);
+            t.equal(libs.exec.b(samples[2]), 23);
+            t.equal(libs.exec.b(samples[3]), 29);
+            t.equal(libs.exec.b(samples[4]), 26);
+            t.end();
+});
