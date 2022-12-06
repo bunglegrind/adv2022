78e2eb384cbf2c93783063d425ee0e49835b5cc2
bunglegrind <bunglegrind@libero.it>
2022-12-06 22:24:09 +0100

    day 7 scaffolding

---
+++ libs/7.js
@@ -0,0 +1,11 @@
+import * as R from "ramda";
+
+const parseInput = R.split("\n");
+
+export default Object.freeze({
+    parseInput,
+    exec: {
+        a: 0,
+        b; 0
+    }
+});
---
+++ test/7.js
@@ -0,0 +1,2 @@
+import test from "tape";
+import libs from "../libs/7.js";
