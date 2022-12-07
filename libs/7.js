2b3d95a53146bed62dfc6064c514ac161225d5a6
bunglegrind <bunglegrind@libero.it>
2022-12-08 00:34:02 +0100

    day 7 completed

--- libs/7.js
+++ libs/7.js
@@ -1,18 +1,69 @@
 import * as R from "ramda";
 
-const parseInput = R.split("\n");
+function command(cmd, tokens) {
+    let parameters = "";
+    let output = "";
+    let exec;
+    if (cmd === "cd") {
+        parameters = R.head(tokens),
+        exec = function (tree) {
+            if (parameters === "..") {
+                return tree.parent();
+            }
+            return tree.getChild(parameters);
+        };
+    }
+    if (cmd === "ls") {
+        output = R.splitEvery(2, tokens);
+        exec = function (tree) {
+            output.forEach(function ([size, name]) {
+                if (size === "dir") {
+                    return tree.addChild({label: name});    
+                }
+                return tree.addChild({size: Number(size), label: name});
+            });
+            return tree;
+        };
+    }
+    return {
+        type: () => cmd,
+        getParameters: () => parameters,
+        getOutput: () => output,
+        exec
+    };
+}
+
+function parseCommand(string) {
+    const tokens = R.pipe(
+        R.split("\n"),
+        R.chain(R.split(" ")),
+        R.map(R.trim),
+        R.reject(R.isEmpty)
+    )(string);
+
+    return command(tokens[0], R.tail(tokens));
+
+}
+
+const parseInput = R.pipe(
+    R.split("$ "),
+    R.tail,
+    R.map(parseCommand)     
+);
+
 
 function tree({label, size = 0, pt}) {
     const children = (!size ? [] : undefined);
     const isDir = () => size === 0;
+    const calcSize = () => (
+        isDir()
+        ? children.reduce((a, n) => a + n.calcSize(), 0)
+        : size
+    );
     const myself = Object.freeze({
         parent: () => pt,
         children,
-        calcSize: () => (
-            isDir()
-            ? children.reduce((a, n) => a + n.calcSize(), 0)
-            : size
-        ),
+        calcSize,
         addChild: function (opts) {
             if (!isDir()) {
                 throw "Cannot add an item to a file";
@@ -22,7 +73,17 @@ function tree({label, size = 0, pt}) {
             return node;
         },
         isDir,
-        getLabel: () => label
+        getLabel: () => label,
+        getChild: (lbl) => children.find((n) => n.getLabel() === lbl),
+        getDirSizes: function () {
+            if (isDir()) {
+                const nodes = R.pipe(
+                    R.filter((x) => x.isDir()),
+                    R.chain((n) => n.getDirSizes())
+                )(children);
+                return [calcSize(), ...R.reject(R.isEmpty)(nodes)];
+            }
+        }
     });
 
     return myself;
@@ -33,8 +94,44 @@ function tree({label, size = 0, pt}) {
 export default Object.freeze({
     parseInput,
     tree,
+    parseCommand,
     exec: {
-        a: 0,
-        b: 0
+        a: function (sample) {
+            const commands = R.tail(parseInput(sample));
+            const mytree = tree({label: "/"});
+            function executeCommands(node, commands) {
+                if (commands.length === 0) {
+                    return node;
+                }
+                return executeCommands(
+                    R.head(commands).exec(node),
+                    R.tail(commands)
+                );
+            }
+            executeCommands(mytree, commands);
+            return R.pipe(
+                R.filter((x) => x <= 100000),
+                R.sum
+            )(mytree.getDirSizes());
+        },
+        b: function (sample) {
+            const commands = R.tail(parseInput(sample));
+            const mytree = tree({label: "/"});
+            function executeCommands(node, commands) {
+                if (commands.length === 0) {
+                    return node;
+                }
+                return executeCommands(
+                    R.head(commands).exec(node),
+                    R.tail(commands)
+                );
+            }
+            executeCommands(mytree, commands);
+            const sizes = mytree.getDirSizes();
+            return R.pipe(
+                R.filter((x) => x >=  30000000 - (70000000 - mytree.calcSize())),
+                R.reduce((a, x) => (a > x ? x : a), 790000000)
+            )(sizes);
+        }
     }
 });
--- test/7.js
+++ test/7.js
@@ -34,7 +34,9 @@ test("creates an empty tree", function (t) {
         "calcSize",
         "addChild",
         "isDir",
-        "getLabel"
+        "getLabel",
+        "getChild",
+        "getDirSizes"
     ]);
     t.equal(tree.getLabel(), "/");
     t.equal(tree.calcSize(), 0);
@@ -58,6 +60,13 @@ test("cannot add an item to a file", function (t) {
         () => tree.children[0].addChild({label: "a"}),
         /^Cannot add an item to a file$/
     );
+    t.end();
+});
+
+test("get a child from label", function (t) {
+    const tree = libs.tree({label: "/"});
+    const node = tree.addChild({label: "opt"});
+    t.equal(tree.getChild("opt"), node);
     t.end();
 });
 
@@ -91,7 +100,89 @@ test("get size of a dir", function (t) {
     t.end();    
 });
 
-test("parse input sample", function (t) {
+test("get all dir sizes", function (t) {
+    const tree = libs.tree({label: "/"});
+    const node = tree.addChild({label: "opt"});
+    node.addChild({label: "a.txt", size: 300});
+    node.addChild({label: "b.txt", size: 300});
+    node.addChild({label: "c.txt", size: 300});
+    const anotherDir = node.addChild({label: "f"});
+    anotherDir.addChild({label: "c.txt", size: 100});
+    tree.addChild({label: "a.txt", size: 300});
+
+    t.deepEqual(tree.getDirSizes(), [1300, 1000, 100]);
     t.end();
 });
 
+test("parse cmd", function (t) {
+    let command;
+
+    command = libs.parseCommand(" cd /\n"); 
+    t.equal(command.type(), "cd");
+    t.equal(command.getParameters(), "/");
+    t.equal(command.getOutput(), "");
+
+    command = libs.parseCommand(" cd a\n"); 
+    t.equal(command.type(), "cd");
+    t.equal(command.getParameters(), "a");
+    t.equal(command.getOutput(), "");
+
+    command = libs.parseCommand(
+        " ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n"
+    ); 
+    t.equal(command.type(), "ls");
+    t.equal(command.getParameters(), "");
+    t.deepEqual(command.getOutput(), [
+        ["dir", "a"],
+        ["14848514", "b.txt"],
+        ["8504156", "c.dat"],
+        ["dir", "d"]
+    ]);
+    t.end();
+});
+
+test("parse input sample", function (t) {
+    const commands = libs.parseInput(sample);
+    t.equal(commands.length, 10);
+    t.equal(commands[0].type(), "cd");
+    t.equal(commands[0].getParameters(), "/");
+    t.equal(commands[0].getOutput(), "");
+    t.equal(commands[4].type(), "cd");
+    t.equal(commands[4].getParameters(), "e");
+    t.equal(commands[4].getOutput(), "");
+    t.equal(commands[7].type(), "cd");
+    t.equal(commands[7].getParameters(), "..");
+    t.equal(commands[7].getOutput(), "");
+    t.equal(commands[9].type(), "ls");
+    t.equal(commands[9].getParameters(), "");
+    t.deepEqual(commands[9].getOutput(), [
+        ["4060174", "j"],
+        ["8033020", "d.log"],
+        ["5626152", "d.ext"],
+        ["7214296", "k"]
+    ]);
+    t.end();
+});
+
+test("build tree from single commands", function (t) {
+    const tree = libs.tree({label: "/"});
+    const node = tree.addChild({label: "a"});
+    t.equal(libs.parseCommand("cd a\n").exec(tree), node);
+    libs.parseCommand("ls \ndir a\n134 b\n").exec(node);
+    const a = node.getChild("a");
+    t.ok(a.isDir());
+    const b = node.getChild("b");
+    t.notOk(b.isDir());
+    t.equal(b.calcSize(), 134);
+    t.end();
+});
+
+test("sample return expect outcome A", function (t) {
+    t.equal(libs.exec.a(sample), 95437);
+    t.end();
+});
+
+test("sample return expect outcome B", function (t) {
+    t.equal(libs.exec.b(sample), 24933642);
+    t.end();
+});
