e4765baf77d2f585bc4980ca0c63ddf78cccb62d
bunglegrind <bunglegrind@libero.it>
2022-12-07 16:49:51 +0100

    day 7 WIP

--- libs/7.js
+++ libs/7.js
@@ -2,10 +2,43 @@ import * as R from "ramda";
 
 const parseInput = R.split("\n");
 
+function tree({label, size = 0, pt}) {
+    const children = (!size ? [] : undefined);
+    const isDir = () => size === 0;
+    const fold = R.curry(function reduce(reducerFn, init, node) {
+        const acc = reducerFn(init, node);
+        if (!node.isDir()) {
+            return acc;
+        }
+        return node.children.reduce(fold(reducerFn), acc);
+    });
+    const myself = Object.freeze({
+        parent: () => pt,
+        children,
+        size,
+        calcSize: () => fold((a, n) => a + n.size, 0, myself),
+        addChild: function (opts) {
+            if (!isDir()) {
+                throw "Cannot add an item to a file";
+            }
+            const node = tree({...opts, pt: myself});
+            children.push(node);
+            return node;
+        },
+        isDir,
+        getLabel: () => label
+    });
+
+    return myself;
+}
+
+
+
 export default Object.freeze({
     parseInput,
+    tree,
     exec: {
         a: 0,
-        b; 0
+        b: 0
     }
 });
--- test/7.js
+++ test/7.js
@@ -24,3 +24,75 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k`;
+
+
+test("creates an empty tree", function (t) {
+    const tree = libs.tree({label: "/"});
+    t.deepEqual(Object.keys(tree), [
+        "parent",
+        "children",
+        "size",
+        "calcSize",
+        "addChild",
+        "isDir",
+        "getLabel"
+    ]);
+    t.equal(tree.getLabel(), "/");
+    t.equal(tree.calcSize(), 0);
+    t.ok(tree.isDir());
+    
+    t.end();
+});
+
+test("creates a tree and add a file", function (t) {
+    const tree = libs.tree({label: "/"});
+    tree.addChild({label: "a.txt", size: 30});
+    t.equal(tree.children[0].getLabel(), "a.txt");
+    t.notOk(tree.children[0].isDir());
+    t.end();
+});
+
+test("cannot add an item to a file", function (t) {
+    const tree = libs.tree({label: "/"});
+    tree.addChild({label: "a.txt", size: 30});
+    t.throws(
+        () => tree.children[0].addChild({label: "a"}),
+        /^Cannot add an item to a file$/
+    );
+    t.end();
+});
+
+test("get parent of a node", function (t) {
+    const tree = libs.tree({label: "/"});
+    const node = tree.addChild({label: "opt"});
+    const file = node.addChild({label: "a.txt", size: 300});
+
+    t.equal(node.parent(), tree);
+    t.equal(file.parent(), node);
+    t.end();
+});
+
+test("get size of a file", function (t) {
+    const tree = libs.tree({label: "/"});
+    const node = tree.addChild({label: "opt"});
+    const file = node.addChild({label: "a.txt", size: 300});
+    t.equal(file.calcSize(), 300);
+    t.end();    
+});
+
+test("get size of a dir", function (t) {
+    const tree = libs.tree({label: "/"});
+    const node = tree.addChild({label: "opt"});
+    node.addChild({label: "a.txt", size: 300});
+    node.addChild({label: "a.txt", size: 300});
+    node.addChild({label: "a.txt", size: 300});
+    tree.addChild({label: "a.txt", size: 300});
+    t.equal(node.calcSize(), 900);
+    t.equal(tree.calcSize(), 1200);
+    t.end();    
+});
+
+test("parse input sample", function (t) {
+    t.end();
+});
+
