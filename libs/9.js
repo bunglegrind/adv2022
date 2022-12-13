2507bd4fb0e260c46f99a4ca214ab0748b8e83b6
bunglegrind <bunglegrind@libero.it>
2022-12-13 13:34:46 +0100

    day 9 completed

--- libs/9.js
+++ libs/9.js
@@ -3,34 +3,104 @@ import * as R from "ramda";
 const parseInput = R.pipe(
     R.split("\n"),
     R.reject(R.isEmpty),
-    R.map(R.pipe(
+    R.chain(R.pipe(
         R.split(" "),
         function ([direction, step]) {
             step = Number(step);
             if (direction === "R") {
-                return [step, 0];
+                return R.repeat([1, 0], step);
             }
             if (direction === "U") {
-                return [0, step];
+                return R.repeat([0, 1], step);
             }
             if (direction === "D") {
-                return [0, -step];
+                return R.repeat([0, -1], step);
             }
             if (direction === "L") {
-                return [-step, 0];
+                return R.repeat([-1, 0], step);
             }
         }
     ))
 );
 
-const start;
+function rope() {
+    const sum = R.zipWith(R.add);
+    const diff = R.zipWith(R.subtract);
+    const squareNorm = R.pipe(R.map((x) => x ** 2), R.sum);
+    const tailHistory = Object.create(null);
+    const history = () => Object.keys(tailHistory).length;
+
+    let h = [0, 0];
+    let t = [0, 0];
+    tailHistory[JSON.stringify(t)] = true;
+    const head = () => h;
+    const tail= () => t;
+    function move(step) {
+        h = sum(h, step);
+        updateTail();
+    }
+
+    function updateHead(head) {
+        h = head;
+        updateTail();
+    }
+
+    const needsToBeUpdated = () => squareNorm(diff(t, h)) > 2;
+
+    function updateTail() {
+        if (needsToBeUpdated()) {
+            t = sum(t, R.map(Math.sign, diff(h, t)));
+            tailHistory[JSON.stringify(t)] = true;
+        }
+    }
+
+    
+
+    return Object.freeze({
+        head,
+        tail,
+        move,
+        updateHead,
+        history
+    });
+}
+
+function ropes(knots) {
+    const array = R.times(rope, knots - 1);
+    const last = () => R.last(array);
+    function move(step) {
+        R.head(array).move(step);
+        R.reduce(function (newHead, rope) {
+            rope.updateHead(newHead);
+            return rope.tail();
+        }, R.head(array).tail(), R.tail(array));
+    }
+
+    return Object.freeze({
+        last,
+        move
+    });
+}
 
 export default Object.freeze({
     parseInput,
-    position,
-    start,
+    rope,
     exec: {
-        a: 0,
-        b: 0
+        a: function (data) {
+            const r = rope();
+            R.pipe(
+                parseInput,
+                R.forEach((x) => r.move(x))
+            )(data);
+            return r.history();
+        },
+        b: function (data) {
+            const r = ropes(10);
+            R.pipe(
+                parseInput,
+                R.forEach((x) => r.move(x))
+            )(data);
+            return r.last().history();
+        }
     }
 });
--- test/9.js
+++ test/9.js
@@ -10,17 +10,71 @@ D 1
 L 5
 R 2`;
 
+const anotherSample = `R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`;
+
 
 test("parse input", function (t) {
-    t.deepEqual(libs.parseInput(sample), [
-        [4, 0], [0, 4], [-3, 0], [0, -1], [4, 0], [0, -1], [-5, 0], [2, 0]
+    t.deepEqual(libs.parseInput("R 2\nU 1\nL 3\nD 1"), [
+        [1, 0], [1, 0], [0, 1], [-1, 0], [-1, 0], [-1, 0], [0, -1]
     ]);
     t.end();
 });
 
-test("move head", function (t) {
-    libs.start();
-    t.deepEqual(libs.position(), [0, 0]);
+test("move", function (t) {
+    const rope = libs.rope();
+    t.deepEqual(rope.head(), [0, 0]);
+    t.deepEqual(rope.tail(), [0, 0]);
+
+    rope.move([1, 0]);
+    t.deepEqual(rope.head(), [1, 0]);
+    t.deepEqual(rope.tail(), [0, 0]);
+
+    rope.move([1, 0]);
+    t.deepEqual(rope.head(), [2, 0]);
+    t.deepEqual(rope.tail(), [1, 0]);
+
+    rope.move([0, 1]);
+    t.deepEqual(rope.head(), [2, 1]);
+    t.deepEqual(rope.tail(), [1, 0]);
+
+    rope.move([0, 1]);
+    t.deepEqual(rope.head(), [2, 2]);
+    t.deepEqual(rope.tail(), [2, 1]);
+
+    rope.move([-1, 0]);
+    t.deepEqual(rope.head(), [1, 2]);
+    t.deepEqual(rope.tail(), [2, 1]);
+
+
     t.end();
 
+});
+
+test("history", function (t) {
+    const rope = libs.rope();
+    rope.move([1, 0]);
+    rope.move([1, 0]);
+    rope.move([0, 1]);
+    rope.move([0, 1]);
+    rope.move([-1, 0]);
+    
+    t.equal(rope.history(), 3);
+
+    t.end();
+});
+test("sample return expect outcome A", function (t) {
+    t.equal(libs.exec.a(sample), 13);
+    t.end();
+});
+
+test("sample return expect outcome B", function (t) {
+    t.equal(libs.exec.b(anotherSample), 36);
+    t.end();
 });
